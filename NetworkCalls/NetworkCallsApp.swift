eca88222d301d2de70da44b33a586a9466d340b3
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-03-17 18:52:18 +0100

    Initial Commit

---
+++ NetworkCalls/NetworkCallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkCallsApp.swift
+//  NetworkCalls
+//
+//  Created by Maziar Layeghkar on 17.03.24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkCallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
